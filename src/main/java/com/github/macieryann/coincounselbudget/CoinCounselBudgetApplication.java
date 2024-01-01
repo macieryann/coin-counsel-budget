d3f302c236ee234e06da9548d4df607ac40c9f2c
macieryann <ryanmacie@yahoo.com>
2024-01-01 14:14:06 -0600

    initial commit for budget app

---
+++ src/main/java/com/github/macieryann/coincounselbudget/CoinCounselBudgetApplication.java
@@ -0,0 +1,14 @@
+package com.github.macieryann.coincounselbudget;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoinCounselBudgetApplication {
+
+	public static void main(String[] args) {
+		System.out.println("!!!!! starting budget app !!!!!");
+		SpringApplication.run(CoinCounselBudgetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/macieryann/coincounselbudget/CoinCounselBudgetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.macieryann.coincounselbudget;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoinCounselBudgetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
